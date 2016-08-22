77a6aa73e11869628278046da62f8c0af4d73e8e
balibou <benjamin.cherion@gmail.com>
2016-08-22 10:37:32 +0200

    refactoring answer2 component

--- imports/ui/Answer2.jsx
+++ imports/ui/Answer2.jsx
@@ -1,38 +1,36 @@
 import React, { Component, PropTypes } from 'react';
 
-export class Answer2 extends Component {
-  render() {
-    let styles = {};
-    if (this.props.questionAnswered && this.props.answer.goodAnswer) {
-      styles = {
-        li: {
-          backgroundColor: 'green',
-        },
-      };
-    } else if (this.props.questionAnswered && !this.props.answer.goodAnswer && this.props.answer.checked) {
-      styles = {
-        li: {
-          backgroundColor: 'red',
-        },
-      };
-    } else {
-      styles = {
-        li: {},
-      };
-    }
-    return (
-      <li style={styles.li}>
-        <input
-          type="radio"
-          name="answer"
-          value={this.props.answer.answerText}
-          onClick={() => this.props.questionAnswered(this.props.answer, this.props.databaseQuestionId)}
-          onChange={() => this.props.checkAnswer(this.props.databaseQuestionId, this.props.questionId)}
-          checked={this.props.answer.checked}
-          disabled={this.props.answer.disabled}
-        />
-        {this.props.answer.answerText}
-      </li>
-    );
+export function Answer2({ questionAnswered, answer, databaseQuestionId, questionId }) {
+  let styles = {};
+  if (questionAnswered && answer.goodAnswer) {
+    styles = {
+      li: {
+        backgroundColor: 'green',
+      },
+    };
+  } else if (questionAnswered && !answer.goodAnswer && answer.checked) {
+    styles = {
+      li: {
+        backgroundColor: 'red',
+      },
+    };
+  } else {
+    styles = {
+      li: {},
+    };
   }
+  return (
+    <li style={styles.li}>
+      <input
+        type="radio"
+        name="answer"
+        value={answer.answerText}
+        onClick={() => this.props.questionAnswered(answer, databaseQuestionId)}
+        onChange={() => this.props.checkAnswer(databaseQuestionId, questionId)}
+        checked={answer.checked}
+        disabled={answer.disabled}
+      />
+      {answer.answerText}
+    </li>
+  );
 }
